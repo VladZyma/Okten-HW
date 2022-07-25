7e920e22618fdbc42edb28f72c4d736b9cd4bdab
VladZyma <vladzima13@gmail.com>
2022-07-25 09:38:20 +0300

    JS HW_1

---
+++ JavaScript/HW_1/script.js
@@ -0,0 +1,87 @@
+//========= 1 ===========
+let a2 = 'hello';
+console.log(a2);
+alert(a2);
+document.write(a2);
+
+let b2 = 'owu';
+console.log(b2);
+alert(b2);
+document.write(b2);
+
+let c2 = 'com';
+console.log(c2);
+alert(c2);
+document.write(c2);
+
+let d = 'ua';
+console.log(d);
+alert(d);
+document.write(d);
+
+let num = 1;
+console.log(num);
+alert(num);
+document.write(num);
+
+let num2 = 10;
+console.log(num2);
+alert(num2);
+document.write(num2);
+
+let num3 = -999;
+console.log(num3);
+alert(num3);
+document.write(num3);
+
+let num4 = 123;
+console.log(num4);
+alert(num4);
+document.write(num4);
+
+let num5 = 3.14;
+console.log(num5);
+alert(num5);
+document.write(num5);
+
+let num6 = 2.7;
+console.log(num6);
+alert(num6);
+document.write(num6);
+
+let num7 = 16;
+console.log(num7);
+alert(num7);
+document.write(num7);
+
+let bool = true;
+console.log(bool);
+alert(bool);
+document.write(bool);
+
+let bool2 = false;
+console.log(bool);
+alert(bool);
+document.write(bool);
+
+//==== 2 ============
+let firstName = 'Vladyslav';
+let midlName = 'Olegovych';
+let lastName = 'Zyma';
+let person = lastName + ' ' + firstName + ' ' + midlName;
+
+//==== 3 ===========
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+//==== 4 ==========
+let name = prompt('Enter your name:');
+let mName = prompt('Enter your midl name:');
+let age = prompt('Enter your age:');
+console.log(name + ' ' + mName + ' ' + age);
